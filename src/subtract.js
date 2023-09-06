4586d0fc76812ef90ba2b1ca84ccd9f3844a8d1b
Martinaroque <martirocklx@hotmail.com>
2023-09-06 10:54:31 +0100

    adding subtract section

--- src/subtract.js
+++ src/subtract.js
@@ -10,7 +10,7 @@ function generate_equation() {
   var allanswer = [];
   var switchAnswer = [];
 
-  answer = num1 + num2;
+  answer = num1 - num2;
   document.getElementById("num1").innerHTML = num1;
   document.getElementById("num2").innerHTML = num2;
 
